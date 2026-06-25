300f88d8bc57fdc34fadb94679e2303b489210ba
Wei Jiang <epichello12456@gmail.com>
2026-06-25 20:19:36 +1000

    back up v2

--- LCDScreen_Stuff.ino
+++ LCDScreen_Stuff.ino
@@ -7,9 +7,27 @@ int button_state = 0;
 int button_count = 0;
 int button_count_memory = 0;
 
+int button_state_current = 0;
+int button_state_previous = 0;
+
+bool button_was_pressed = false;
+
 LiquidCrystal_I2C lcd(0x27, 16,2); //16 wide 2 lines
 
-void pressed(int &button_count, int &button_count_memory)
+bool lever_function(int &button_state_current, int &button_state_previous, int&button_count)
+{
+  if(button_state_current == button_state_previous)
+  {
+    return false;
+  }
+  if(button_state_current == 1 && button_state_previous == 0)
+  {
+    button_count++;
+    return true;
+  }
+}
+
+void off_on_lcd(int &button_count, int &button_count_memory)
 {
   if(button_count - button_count_memory == 1)
   {
@@ -33,14 +51,17 @@ lcd.noBacklight(); //initialise with no backlight
 
 void loop()
 {
-
 button_state = digitalRead(button_pin); //returns 1 (up) or 0 (down)
-if(button_state == LOW)
+button_state_previous = button_state;
+if(button_state == 0)
 {
-  button_count++;
-  // pressed(button_count, button_count_memory);
-  Serial.println(button_count);
-  Serial.println("bing");
-
+  button_state_current = button_state;
+  Serial.println("hello1");
+}
+button_was_pressed = lever_function(button_state_current, button_state_previous, button_count) ;
+if(button_was_pressed == true)
+{
+  off_on_lcd(button_count, button_count_memory);
+  Serial.println("hello2");
 }
 }
