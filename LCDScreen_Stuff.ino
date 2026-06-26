870047beca3667ea44933e81c56a79abb41c1209
Wei Jiang <epichello12456@gmail.com>
2026-06-26 11:16:16 +1000

    button works like lever + lcd screen works

--- LCDScreen_Stuff.ino
+++ LCDScreen_Stuff.ino
@@ -3,41 +3,38 @@
 
 const int button_pin = 2;
 
-int button_state = 0;
+int button_state = HIGH;  //initial state
 int button_count = 0;
-int button_count_memory = 0;
 
-int button_state_current = 0;
-int button_state_previous = 0;
+int button_state_previous = HIGH;
 
-bool button_was_pressed = false;
+unsigned long last_debounce_time = 0; //unsigned long since we're counting in milliseconds
+unsigned long debounce_delay = 200;   //which would fill up super fast with int type
 
 LiquidCrystal_I2C lcd(0x27, 16,2); //16 wide 2 lines
 
 bool lever_function(int &button_state_current, int &button_state_previous, int&button_count)
 {
-  if(button_state_current == button_state_previous)
+  if(button_state_current == LOW && button_state_previous == HIGH)  //checks for edge case 
   {
-    return false;
-  }
-  if(button_state_current == 1 && button_state_previous == 0)
-  {
-    button_count++;
+    if ((millis() - last_debounce_time) > debounce_delay) //millis returns the ms since arduino started
+    {                                                     //minus the last time it was activated 
+    last_debounce_time = millis(); 
     return true;
+    }
   }
+  return false;
 }
 
-void off_on_lcd(int &button_count, int &button_count_memory)
+void update_lcd(int &button_count)
 {
-  if(button_count - button_count_memory == 1)
+  if(button_count%2 == 1)
   {
     lcd.backlight();
     return;
-  }
-  if(button_count - button_count_memory == 2)
+  } else
   {
     lcd.noBacklight();
-    button_count_memory = button_count;
     return;
   }
 }
@@ -45,23 +42,24 @@ void off_on_lcd(int &button_count, int &button_count_memory)
 void setup() 
 {
 Serial.begin(9600);
-pinMode(button_pin, INPUT_PULLUP); //sets 
+pinMode(button_pin, INPUT_PULLUP); //sets input to up
+lcd.init();
 lcd.noBacklight(); //initialise with no backlight
+
+lcd.setCursor(0, 0);             
+lcd.print("  (*(*(*-*)*)*)");      // Print text to the first line
+lcd.setCursor(0, 1);             // Move the cursor to column 0, row 1 (Second line)
+lcd.print("   MEEP MEEP");    // Print text to the second line
+
 }
 
 void loop()
 {
-button_state = digitalRead(button_pin); //returns 1 (up) or 0 (down)
 button_state_previous = button_state;
-if(button_state == 0)
+button_state = digitalRead(button_pin); //returns 1 (up) or 0 (down)
+if(lever_function(button_state, button_state_previous, button_count))
 {
-  button_state_current = button_state;
-  Serial.println("hello1");
-}
-button_was_pressed = lever_function(button_state_current, button_state_previous, button_count) ;
-if(button_was_pressed == true)
-{
-  off_on_lcd(button_count, button_count_memory);
-  Serial.println("hello2");
+  button_count++;
+  update_lcd(button_count);
 }
 }
